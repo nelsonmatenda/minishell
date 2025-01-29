86ec83b9479313d23d112c9a824451362058c0c9
Nelson Matenda <matendafigueiredo@hotmail.com>
2025-01-29 20:10:49 +0100

    feat: siganls, lidando com os sinais ctrl+c, ctrl+d, ctrl+/

--- .objs/src/minishell.d
+++
@@ -1,5 +0,0 @@
-.objs/src/minishell.o: src/minishell.c src/../includes/minishell.h \
- src/../includes/../libft/libft.h src/../includes/error_msg.h
-src/../includes/minishell.h:
-src/../includes/../libft/libft.h:
-src/../includes/error_msg.h:
--- .objs/src/tokens/get_paths.d
+++
@@ -1,7 +0,0 @@
-.objs/src/tokens/get_paths.o: src/tokens/get_paths.c \
- src/tokens/../../includes/minishell.h \
- src/tokens/../../includes/../libft/libft.h \
- src/tokens/../../includes/error_msg.h
-src/tokens/../../includes/minishell.h:
-src/tokens/../../includes/../libft/libft.h:
-src/tokens/../../includes/error_msg.h:
